95d94810d41594317b51267818e3bb34d1d732cc
Gonda Gábor <energyteamgazuru@gmail.com>
2023-02-24 23:01:22 +0100

    Initial commit

---
+++ src/test/java/hu/bme/aut/saturn/SaturnApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.saturn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaturnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
