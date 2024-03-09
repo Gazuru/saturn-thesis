6260e6889c812de3c2998ac3438bc51389bd2123
Gonda Gábor <energyteamgazuru@gmail.com>
2024-03-09 18:22:39 +0100

    Enable security, fix ComponentScan

--- saturn-education/saturn-education-application/src/main/java/hu/bme/aut/saturn/education/application/SaturnEducationApplication.java
+++ saturn-education/saturn-education-application/src/main/java/hu/bme/aut/saturn/education/application/SaturnEducationApplication.java
@@ -2,8 +2,10 @@ package hu.bme.aut.saturn.education.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "hu.bme.aut.saturn.education")
 public class SaturnEducationApplication {
 
 	public static void main(String[] args) {
--- saturn-management/saturn-management-application/src/main/java/hu/bme/aut/saturn/management/application/SaturnManagementApplication.java
+++ saturn-management/saturn-management-application/src/main/java/hu/bme/aut/saturn/management/application/SaturnManagementApplication.java
@@ -2,8 +2,10 @@ package hu.bme.aut.saturn.management.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "hu.bme.aut.saturn.management")
 public class SaturnManagementApplication {
 
 	public static void main(String[] args) {
